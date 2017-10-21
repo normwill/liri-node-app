ba6b2fbdc9073b3e5ff14666e3f3a48ff0cc9b4a
Norman Allen <normz@Normans-MacBook-Pro.local>
2017-10-20 21:53:16 -0400

    not finished and need help

---
+++ keys.js
@@ -0,0 +1,20 @@
+//Twitter Keys
+
+var twitterKeys = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
+}
+
+//Spotify Keys 
+var spotifyKeys = {
+  id: "",
+  secret: ""
+};
+
+// Exporting the Keys
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys 
+};
